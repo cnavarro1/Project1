beb7c7414717f1afc91c2fbe3266ee27a3573339
cnavarro1 <cnavarro1@hawk.iit.edu>
2022-04-13 00:47:36 -0500

    So many changes

--- js/map.js
+++ js/map.js
@@ -1,35 +1,27 @@
-function init() {
-  var el = document.getElementById('canvas');
-  var myLocation = new google.maps.LatLng(41.835117, -87.627130);
-  var mapOptions = {
-    center: myLocation,
-    zoom: 18,
-    mapTypeId: google.maps.MapTypeId.SATELLITE,
-    mapTypeControlOptions: {
-      position: google.maps.ControlPosition.BOTTOM_Center
-    }
+var myMap;
+function initMap() {
+  alert('it works');
+  myMap = new google.maps.Map(document.getElementById('myMap'), {
+    center: {lat: 41.835471, lng: -87.625888},
+    zoom: 16
   });
-  
-  var myMap = new google.maps.Map(el, mapOptions);
+ 
   
   var marker = new google.maps.Marker({
-    position: myLocation,
+    position: new google.maps.LatLng(41.835471, -87.625888),
     map: myMap,
     animation: google.maps.Animation.BOUNCE,
     icon: 'images/marge.png'
   });
   
-  var contentString = '<h1>This Place</h1><p>This is a place</p>'
+  var contentString = '<h1>This Place</h1><p>This is a place</p>';
   
   var infoWindow = new google.maps.InfoWindow({
       content: contentString
-    });
+  });
   
   google.maps.event.addListener(marker, 'mouseover', function() {
       infowindow.open(myMap, marker);
-    });
-    
-    
+  });
 }
-
-google.maps.event.addDomListener(window, 'load', init);
+google.maps.event.addDomListener(window, 'load', initMap;
